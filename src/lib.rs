01ddbd97c13c50e5e4e37378a3d9603a17f1f79a
berkingurcan <berkingurcan@yandex.com>
2022-05-26 10:16:16 +0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,40 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::{near_bindgen, env};
 use near_sdk::payable;
-use near_sdk::{AccountId, Balance, Duration, Gas, Timestamp};
+use near_sdk::{AccountId, Balance};
 use near_sdk::{Promise, PromiseResult};
 
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
 pub struct Contract {
     // SETUP CONTRACT STATE
+    let deposit_map: UnorderedMap::new(b"a".to_vec());
 }
 
 #[near_bindgen]
 impl Contract {
+
     #[payable]
-    pub fn deposit(amount: U128, to: AccountId) -> Promise {
-        Promise::new(this).transfer(amount.0)
+    pub fn deposit() {
+        let account_id = env::signer_account_id();
+        let current_value = deposit_map.get(&account_id).unwrap_or(0);
+        deposit_map.insert(&account_id, &(current_value + env::attached_deposit()));
+    }
+
+    #[payable]
+    pub fn withdraw(amount: U128) -> Promise {
+        let account_id = env::signer_account_id();
+        let current_value = deposit_map.get(&account_id).unwrap_or(0);
+        let amount_to_withdraw: Balance = amount.into();
+        
+        assert!(
+            (amount_to_withdraw <= current_value) && (amount_to_withdraw > 0),
+            "Amount of withdrawal is invalid",
+        );
+        
+        Promise::new(env::signer_account_id()).transfer(amount_to_withdraw)
     }
 }
-
-/*
- * the rest of this file sets up unit tests
- * to run these, the command will be:
- * cargo test --package rust-template -- --nocapture
- * Note: 'rust-template' comes from Cargo.toml's 'name' key
- */
 
 // use the attribute below for unit tests
 #[cfg(test)]
